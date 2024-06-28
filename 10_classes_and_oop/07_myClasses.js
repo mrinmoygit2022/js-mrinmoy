a7863ba58ec25e0b9b7b579884ac425d80c1cc91
mrinmoygithub <mrinmoy.r@xelpmoc.in>
2024-06-28 17:55:19 +0530

    bind class

--- 10_classes_and_oop/07_myClasses.js
+++ 10_classes_and_oop/07_myClasses.js
@@ -21,6 +21,8 @@ const chai = new User("chai", "chai@google.com", "123");
 console.log(chai.encryptPassword());
 console.log(chai.changeUsername());
 
+// behind the scene
+
 function User(username, email, password){
     this.username = username;
     this.email = email;
@@ -29,6 +31,10 @@ function User(username, email, password){
 
 User.prototype.encryptPassword = function(){
     return `${this.password}abc`
+}
+
+User.prototype.changeUsername = function(){
+    return `${this.username.toUpperCase()}`
 }
 
 const tea = new User("tea", "tea@google.com", "123");
---
+++ 10_classes_and_oop/09_staticprop.js
@@ -0,0 +1,29 @@
+// static properties
+class User{
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username ${this.username}`);
+    }
+
+    static createId(){
+        return `123`
+    }
+}
+
+const Mrinmoy = new User("Mrinmoy")
+// console.log(Mrinmoy.createId());
+
+class Teacher extends User{
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+
+iphone.logMe();
+console.log(iphone.createId());
